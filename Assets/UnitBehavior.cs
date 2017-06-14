74c3306fc07db67d49ec331365469a0f491c5db5
R. Stephens Summerlin <st.summerlin@gmail.com>
2017-06-13 23:13:08 -0400

    Added jumping cubes

---
+++ Assets/UnitBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class UnitBehavior : MonoBehaviour
+{
+
+    private Rigidbody myRigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+        StartCoroutine(JumpAround());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ;
+    }
+
+    IEnumerator JumpAround()
+    {
+        while (true)
+        {
+            Vector3 dir = Random.insideUnitCircle;
+            dir.z = 1;
+            myRigidBody.AddForce(dir * 120);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
